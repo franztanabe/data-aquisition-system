5ccb7aab4a589e42a07a2edd3352b7f8f63f067d
franztanabe <109708002+franztanabe@users.noreply.github.com>
2024-12-06 23:52:59 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <cstdlib>
+
+// Declaração da função run_server que será definida em async_echo_server.cpp
+int run_server(int port);
 
 int main(int argc, char* argv[]) {
-    return 0;
+    if (argc != 2) {
+        std::cerr << "Usage: data_server <port>\n";
+        return 1;
+    }
+
+    int port = std::atoi(argv[1]);
+    return run_server(port);
 }
